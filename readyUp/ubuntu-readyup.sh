95fd4d084e651073d34be3844f37f62cc9c56612
Eric <naisanza@gmail.com>
2017-03-28 16:00:37 -0400

    Disabled cleanup
    Missed the chrome cleanup

--- readyUp/ubuntu-readyup.sh
+++ readyUp/ubuntu-readyup.sh
@@ -36,7 +36,7 @@ chrome=chrome.deb
 if [ ! -f $apps/$chrome ]; then
 	wget -O $apps/$chrome 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
 	dpkg -i $apps/$chrome
-	rm $chrome
+	#rm $apps/$chrome
 else
 	dpkg -i $apps/$chrome
 fi
