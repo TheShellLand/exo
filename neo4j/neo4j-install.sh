0d61fef41f52ccd8163e639ba56b1a06e5cf4eb7
Eric <naisanza@gmail.com>
2017-03-31 15:36:51 -0400

    Added config parameters
    The following will be the default listening address and java memory usage:
    
    dbms.connectors.default_listen_address=0.0.0.0
    dbms.memory.heap.max_size=512m

--- neo4j/neo4j-install.sh
+++ neo4j/neo4j-install.sh
@@ -2,14 +2,16 @@
 
 # Install Neo4j from repository
 
+max_java_mem="512m"
+
 wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb https://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
 apt-get update
 apt-get install -y neo4j
 
 ip r
-read -p "Press Enter"
 
-vi /etc/neo4j/neo4j.conf
+echo "dbms.connectors.default_listen_address=0.0.0.0"  >> /etc/neo4j/neo4j.conf
+echo "dbms.memory.heap.max_size="$max_java_mem"" >> /etc/neo4j/neo4j.conf
 
 service neo4j start
