d14a89086a879531322e35b9687cd515175b3809
Eric <naisanza@gmail.com>
2017-03-31 15:38:37 -0400

    Added netstat watcher

--- neo4j/neo4j-install.sh
+++ neo4j/neo4j-install.sh
@@ -15,3 +15,5 @@ echo "dbms.connectors.default_listen_address=0.0.0.0"  >> /etc/neo4j/neo4j.conf
 echo "dbms.memory.heap.max_size="$max_java_mem"" >> /etc/neo4j/neo4j.conf
 
 service neo4j restart
+
+watch "netstat -ant | grep LISTEN"
