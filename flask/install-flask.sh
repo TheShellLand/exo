f7fe6fd147313389a8f71c2c6afd77b989256fc9
Eric <naisanza@gmail.com>
2017-04-06 01:09:45 -0400

    Missing module

--- flask/install-flask.sh
+++ flask/install-flask.sh
@@ -4,7 +4,7 @@
 
 apt install -y python3-pip
 
-pips="flask flask_login flask_wtf bcrypt neo4j-driver"
+pips="requests flask flask_login flask_wtf bcrypt neo4j-driver"
 
 for package in $pips; do
   pip install $package
