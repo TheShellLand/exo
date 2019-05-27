59decab3b0eb3ffa3b20a6eff70bdeda1595527f
_spaceb <ericjaw@gmail.com>
2019-05-27 17:38:52 -0400

    Create config.sh

---
+++ git/config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# set git global configs
+git config --global core.editor "vim"
+git config --global core.pager "less -r"
+git config --global gpg.program gpg
+git config --global commit.gpgsign true
