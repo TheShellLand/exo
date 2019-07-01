0f8f5add1fca03c1f6239c9902117e2566c402ac
ainiml <naisanza@gmail.com>
2019-07-01 14:39:35 -0400

    added elementaryos ui changes

---
+++ elementaryos/fixes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# annoying elementary os ui
+
+gsettings set io.elementary.terminal.settings natural-copy-paste false
+gsettings set io.elementary.files.preferences single-click false
+
