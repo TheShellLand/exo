34ab626390bfcbb8457b3eb2240bed856f06ee77
_spaceb <ericjaw@gmail.com>
2020-02-22 18:34:05 -0500

    Update and rename config.sh to git-config.sh

--- git/git-config.sh
+++ git/git-config.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # set git global configs
 
-set -x
+read -p "Enter git username: " user
+read -p "Enter git email: " email
 
+set -xe
+
+git config --global user.name "$user"
+git config --global user.email "$email"
 git config --global core.editor "vim"
 git config --global core.pager "less -r"
 git config --global gpg.program gpg
