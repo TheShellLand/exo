0038fc8216fb468017709e8bfbd74a02bcef2ff7
_spaceb <ericjaw@gmail.com>
2020-02-22 18:31:43 -0500

    Update config-nosign.sh

--- git/config-nosign.sh
+++ git/config-nosign.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # set git global configs
 
-set -x 
+set -xe
 
-read -p "Enter git username: " user
-read -p "Enter git email: " email
+read -p 'Enter git username: ' user
+read -p 'Enter git email: ' email
 
 git config --global user.name "$user"
 git config --global user.email "$email"
