a68fa310b68cd7fb29a07e4ed8b62071c15f8093
Eric <58240560+naisanzaa@users.noreply.github.com>
2020-04-28 17:51:17 -0400

    Create git-config.sh

---
+++ git/git-config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# set git global configs
+
+read -p "Enter git username: " user
+read -p "Enter git email: " email
+
+set -xe
+
+git config --local user.name "$user"
+git config --local user.email "$email"
+git config --global core.editor "vim"
+git config --global core.pager "less -r"
+git config --global gpg.program gpg
+git config --global commit.gpgsign true
