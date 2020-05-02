8b19841c5793d691eec42c6cb3e48f736a8d6995
Eric <58240560+naisanzaa@users.noreply.github.com>
2020-05-02 18:04:41 -0400

    Create o.sh

---
+++ progressbars/o.sh
@@ -0,0 +1,30 @@
+echo -en "\r                                   "
+echo -en "\rConnecting [o       ]"
+sleep 0.1
+echo -en "\rConnecting [ o      ]"
+sleep 0.1
+echo -en "\rConnecting [  o     ]"
+sleep 0.1
+echo -en "\rConnecting [   o    ]"
+sleep 0.1
+echo -en "\rConnecting [    o   ]"
+sleep 0.1
+echo -en "\rConnecting [     o  ]"
+sleep 0.1
+echo -en "\rConnecting [      o ]"
+sleep 0.1
+echo -en "\rConnecting [       o]"
+sleep 0.1
+echo -en "\rConnecting [      o ]"
+sleep 0.1
+echo -en "\rConnecting [     o  ]"
+sleep 0.1
+echo -en "\rConnecting [    o   ]"
+sleep 0.1
+echo -en "\rConnecting [   o    ]"
+sleep 0.1
+echo -en "\rConnecting [  o     ]"
+sleep 0.1
+echo -en "\rConnecting [ o      ]"
+sleep 0.1
+echo -en "\r                                   "
