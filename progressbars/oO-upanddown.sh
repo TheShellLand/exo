706b4998c247cf4e0932d5bd919da934d2ebb822
_spaceb <ericjaw@gmail.com>
2020-05-27 12:38:00 -0400

    Update oO-upanddown.sh

--- progressbars/oO-upanddown.sh
+++ progressbars/oO-upanddown.sh
@@ -2,16 +2,16 @@ progress()
 {
   tput civis
   echo -ne '\r.'
-  sleep 0.4
+  sleep 0.2
   echo -ne '\r.o'
-  sleep 0.4
+  sleep 0.2
   echo -ne '\r.oO'
-  sleep 0.4
+  sleep 0.5
   echo -ne '\r.o '
-  sleep 0.4
+  sleep 0.2
   echo -ne '\r.  '
-  sleep 0.4
+  sleep 0.2
   echo -ne '\r   '
-  sleep 0.4
+  sleep 0.2
   tput el
 }
