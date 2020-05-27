e2c80d5bd8b24d12e80ac381b0ea1b025d1c6117
_spaceb <ericjaw@gmail.com>
2020-05-27 12:24:10 -0400

    Update o.sh

--- progressbars/o.sh
+++ progressbars/o.sh
@@ -1,30 +1,36 @@
-echo -en "\r                                   "
-echo -en "\rConnecting [o       ]"
-sleep 0.1
-echo -en "\rConnecting [ o      ]"
-sleep 0.1
-echo -en "\rConnecting [  o     ]"
-sleep 0.1
-echo -en "\rConnecting [   o    ]"
-sleep 0.1
-echo -en "\rConnecting [    o   ]"
-sleep 0.1
-echo -en "\rConnecting [     o  ]"
-sleep 0.1
-echo -en "\rConnecting [      o ]"
-sleep 0.1
-echo -en "\rConnecting [       o]"
-sleep 0.1
-echo -en "\rConnecting [      o ]"
-sleep 0.1
-echo -en "\rConnecting [     o  ]"
-sleep 0.1
-echo -en "\rConnecting [    o   ]"
-sleep 0.1
-echo -en "\rConnecting [   o    ]"
-sleep 0.1
-echo -en "\rConnecting [  o     ]"
-sleep 0.1
-echo -en "\rConnecting [ o      ]"
-sleep 0.1
-echo -en "\r                                   "
+progress()
+{
+  tput civis
+  echo -en "\r"
+  tput el
+  sleep 0.1
+  echo -en "\rConnecting [o       ]"
+  sleep 0.1
+  echo -en "\rConnecting [ o      ]"
+  sleep 0.1
+  echo -en "\rConnecting [  o     ]"
+  sleep 0.1
+  echo -en "\rConnecting [   o    ]"
+  sleep 0.1
+  echo -en "\rConnecting [    o   ]"
+  sleep 0.1
+  echo -en "\rConnecting [     o  ]"
+  sleep 0.1
+  echo -en "\rConnecting [      o ]"
+  sleep 0.1
+  echo -en "\rConnecting [       o]"
+  sleep 0.1
+  echo -en "\rConnecting [      o ]"
+  sleep 0.1
+  echo -en "\rConnecting [     o  ]"
+  sleep 0.1
+  echo -en "\rConnecting [    o   ]"
+  sleep 0.1
+  echo -en "\rConnecting [   o    ]"
+  sleep 0.1
+  echo -en "\rConnecting [  o     ]"
+  sleep 0.1
+  echo -en "\rConnecting [ o      ]"
+  sleep 0.1
+  tput cnorm
+}
