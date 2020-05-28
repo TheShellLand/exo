3527145981f06a7900bf92134146ea20773de5a1
ainiml <naisanza@gmail.com>
2020-05-27 23:08:58 -0400

    update oO-upanddown.sh

--- progressbars/oO-upanddown.sh
+++ progressbars/oO-upanddown.sh
@@ -1,17 +1,23 @@
 progress()
 {
   tput civis
+  echo -ne '\r'
+  tput el
+  sleep 0.2
   echo -ne '\r.'
   sleep 0.2
   echo -ne '\r.o'
   sleep 0.2
   echo -ne '\r.oO'
   sleep 0.5
-  echo -ne '\r.o '
-  sleep 0.2
-  echo -ne '\r.  '
-  sleep 0.2
-  echo -ne '\r   '
-  sleep 0.2
+  echo -ne '\r.o'
   tput el
+  sleep 0.2
+  echo -ne '\r.'
+  tput el
+  sleep 0.2
+  echo -ne '\r'
+  tput el
+  sleep 0.2
+  tput cnorm
 }
