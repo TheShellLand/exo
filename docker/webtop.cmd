31a76458b383c296553fae98b9d18b8380245cd9
naisanzaa <naisanza@gmail.com>
2022-07-03 04:54:43 -0700

    add webtop.cmd

--- docker/webtop.cmd
+++ docker/webtop.cmd
@@ -1,1 +1,2 @@
-docker run --rm -it -p 3000:3000 --shm-size="8gb" ghcr.io/linuxserver/webtop
+docker rm -f webtop
+docker run --name webtop --rm -it -p 3000:3000 --shm-size="8gb" ghcr.io/linuxserver/webtop
