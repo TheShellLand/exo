d37b1ee72d07fa04119f46fcb65a60d3ec4c2691
laerfulaolun <laerfulaolun@gmail.com>
2024-03-29 12:15:48 -0700

    mac: fix acceleration command

--- mac/disable-mouse-acceleration.sh
+++ mac/disable-mouse-acceleration.sh
@@ -3,5 +3,6 @@
 set -xe
 
 #defaults write .GlobalPreferences com.apple.mouse.scaling -1
+defaults write NSGlobalDomain com.apple.mouse.scaling -float "-1"
 defaults write NSGlobalDomain com.apple.mouse.linear -bool "false"
 #defaults write NSGlobalDomain com.apple.mouse.linear -bool "true"
