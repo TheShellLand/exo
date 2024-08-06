fec7b3fb97a9f28769e0dd1a4ef4efae9eac29ed
naisanzaa <naisanza@gmail.com>
2024-08-06 00:12:52 -0700

    fix openvpn-start-nftables.sh to check for different interface

--- bash/openvpn-start-nftables.sh
+++ bash/openvpn-start-nftables.sh
@@ -15,11 +15,35 @@ function start_nftables {
 }
 
 
-function interface_check {
-  echo "interface_check :: checking"
+function interface_wlan0 {
+  echo "interface_wlan0 :: checking"
   ip link show wlan0
   ip link show wlan0 | grep UP
   return $?
+}
+
+
+function interface_ens3 {
+  echo "interface_ens3 :: checking"
+  ip link show ens3
+  ip link show ens3 | grep UP
+  return $?
+}
+
+
+function interface_check {
+  echo "interface_check :: checking"
+  if interface_wlan0; then
+    echo "interface_wlan0 :: OK"
+    return 0
+  fi
+
+  if interface_ens3; then
+    echo "interface_ens3 :: OK"
+    return 0
+  fi
+
+  return 1
 }
 
 
@@ -31,7 +55,7 @@ if interface_check; then
   echo "interface_check :: OK"
   if start_nftables; then
     echo "start_nftables :: OK"
-    exit $?
+    exit 0
   fi
 fi
 
