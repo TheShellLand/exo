7510214678003b61d15406f4694b3e89cc34e382
naisanzaa <naisanza@gmail.com>
2024-08-06 00:49:52 -0700

    better logging

--- bash/ssh-reverse-proxy-dorapisr.sh
+++ bash/ssh-reverse-proxy-dorapisr.sh
@@ -10,7 +10,7 @@ cd "$(dirname $0)"
 REVERSE_PROXY_PORT=2224
 
 function ssh_reverse_proxy {
-  echo "ssh_reverse_proxy :: connecting"
+  echo "ssh :: ssh_reverse_proxy :: connecting"
   ssh -R $REVERSE_PROXY_PORT:localhost:22 -L $REVERSE_PROXY_PORT:localhost:22 user@dream.n7sa.com -CN &
   echo $! > SSH_PID
   return $?
@@ -19,15 +19,15 @@ function ssh_reverse_proxy {
 
 function pid_check {
   if [ -f SSH_PID ]; then
-    echo "pid_check :: checking"
-    ps -fp $(cat SSH_PID)
+    echo -n "ssh :: pid_check :: checking :: "
+    ps -fp $(cat SSH_PID) | grep $(cat SSH_PID)
   fi 
   return $?
 }
 
 
 function ssh_check {
-  echo "ssh_check :: checking"
+  echo -n "ssh :: ssh_check :: checking :: "
   ssh -A user@localhost -p $REVERSE_PROXY_PORT "ssh user@localhost -p $REVERSE_PROXY_PORT id"
   return $?
 }
@@ -35,7 +35,7 @@ function ssh_check {
 
 function pid_kill {
   if [ -f SSH_PID ]; then
-    echo "pid_kill :: killing"
+    echo "ssh :: pid_kill :: killing"
     kill -9 $(cat SSH_PID)
   fi 
   return $?
@@ -47,18 +47,18 @@ if ! pid_check; then
     sleep 1
   done
 
-  echo "ssh_reverse_proxy :: OK"
+  echo "ssh :: ssh_reverse_proxy :: OK"
 
 else
 
   if ! ssh_check; then
-    echo "ssh_check :: FAIL"
+    echo "ssh :: ssh_check :: FAIL"
 
     if pid_kill; then
-      echo "pid_kill :: OK"
+      echo "ssh :: pid_kill :: OK"
 
       if ssh_reverse_proxy; then
-        echo "ssh_reverse_proxy :: OK"
+        echo "ssh :: ssh_reverse_proxy :: OK"
       fi
 
     fi
