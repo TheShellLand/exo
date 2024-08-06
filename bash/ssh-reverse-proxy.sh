1e396c82eb32d46116a9aee3f645f6e8a3423371
naisanzaa <naisanza@gmail.com>
2024-08-06 11:08:08 -0700

    update logging

--- bash/ssh-reverse-proxy.sh
+++ bash/ssh-reverse-proxy.sh
@@ -29,9 +29,9 @@ function pid_check {
 
 
 function ssh_check {
-  echo -n "ssh :: ssh_check :: checking :: "
+  echo -n "ssh :: ssh_check :: checking :: remote hostname :: "
 #  ssh -A user@localhost -p $REVERSE_PROXY_PORT "ssh user@localhost -p $REVERSE_PROXY_PORT id"
-  ssh -A user@localhost -p $REVERSE_PROXY_PORT "id"
+  ssh -A user@localhost -p $REVERSE_PROXY_PORT "hostname"
   return $?
 }
 
