44bf3a916ae78ffb0db4f56fefd478146347d075
naisanzaa <naisanza@gmail.com>
2024-10-19 09:49:26 +0800

    update ssh-reverse-proxy.sh

--- bash/ssh-reverse-proxy.sh
+++ bash/ssh-reverse-proxy.sh
@@ -22,9 +22,9 @@ fi
 
 function ssh_reverse_proxy {
   echo -n "ssh :: ssh_reverse_proxy :: command :: "
-  echo "ssh -R $REVERSE_PROXY_PORT:localhost:22 -L $REVERSE_PROXY_PORT:localhost:22 user@208.113.131.115 -CN &"
+  echo "ssh -R $REVERSE_PROXY_PORT:localhost:22 -L $REVERSE_PROXY_PORT:localhost:22 user@208.113.134.207 -CN &"
   echo -n "ssh :: ssh_reverse_proxy :: connecting :: "
-  ssh -R $REVERSE_PROXY_PORT:localhost:22 -L $REVERSE_PROXY_PORT:localhost:22 user@208.113.131.115 -CN &
+  ssh -R $REVERSE_PROXY_PORT:localhost:22 -L $REVERSE_PROXY_PORT:localhost:22 user@208.113.134.207 -CN &
   echo $! > SSH_PID
   echo "PID $(cat SSH_PID)"
   return $?
