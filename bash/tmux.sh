93357e18fb67efe1aebb17547adbf33bd1f49a0f
naisanzaa <naisanza@gmail.com>
2025-09-25 10:20:13 +0000

    updated tmux

--- bash/tmux.sh
+++ bash/tmux.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-set +x 
+set -xe
 
 mkdir -p ~/.config/tmux/plugins/catppuccin || :
 git clone https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux || :
@@ -12,6 +12,8 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || :
 cp -v .tmux.conf ~/
 
 tmux source ~/.tmux.conf
+
+fc-cache -f || :
 
 echo done.
 
