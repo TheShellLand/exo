a6d58b60fb43acb5cf06ea0a0141122a7d30b21e
naisanzaa <naisanza@gmail.com>
2025-09-25 09:58:40 +0000

    added tmux

---
+++ bash/tmux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+cd "$(dirname "$0")"
+
+set +x 
+
+mkdir -p ~/.config/tmux/plugins/catppuccin || :
+git clone https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux || :
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || :
+
+cp -v .tmux.conf ~/
+
+echo done.
+
